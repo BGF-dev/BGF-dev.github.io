9f79c5d94383a7f288610182f54568904185a796
CBrunain <christophe@brunain.be>
2021-04-12 22:16:02 +0000

    Base template WIP

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  // Output directory: _site
+
+  // Copy `/static/` to `_site/static`
+  eleventyConfig.addPassthroughCopy("static");
+  
+  // Copy `css/fonts/` to `_site/css/fonts`
+  // If you use a subdirectory, it’ll copy using the same directory structure.
+  //eleventyConfig.addPassthroughCopy("css/fonts");
+};
---
+++ en/index.md
@@ -0,0 +1,6 @@
+---
+layout: simplified-home.njk
+title: Brussels Games Festival
+translationKey: simplified-homepage
+---
+# {{ title }} EN
