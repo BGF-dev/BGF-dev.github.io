f60894b948511df3823eb29055d7d308a7adbed0
CBrunain <christophe@brunain.be>
2021-04-13 14:46:07 +0000

    Splashscreen added

--- .eleventy.js
+++ .eleventy.js
@@ -7,4 +7,9 @@ module.exports = function(eleventyConfig) {
   // Copy `css/fonts/` to `_site/css/fonts`
   // If you use a subdirectory, it’ll copy using the same directory structure.
   //eleventyConfig.addPassthroughCopy("css/fonts");
+  return {
+    dir: {
+      'output': '../BGF-build'
+    }
+  };
 };
---
+++ index.md
@@ -0,0 +1,3 @@
+---
+layout: splashscreen.njk
+---
