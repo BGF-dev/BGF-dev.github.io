af97420c75f3c7408153c5b57fee929fcee22531
CBrunain <christophe@brunain.be>
2021-05-03 14:58:16 +0000

    New background color, some accessibility improvements

--- static/main.js
+++ static/main.js
@@ -19233,7 +19233,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32941" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42413" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
