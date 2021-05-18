b74497ed10367ad368e6b0527e176940f96356b0
CBrunain <christophe@brunain.be>
2021-05-18 10:05:18 +0000

    Full site structure + simplified site translation from google

--- static/main.js
+++ static/main.js
@@ -8185,7 +8185,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39353" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41213" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
