73b55473b1626f7788e55a2899225d831b2cb7c8
CBrunain <christophe@brunain.be>
2021-05-20 14:28:35 +0000

    Official translations

--- static/main.js
+++ static/main.js
@@ -8185,7 +8185,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41213" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46501" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
